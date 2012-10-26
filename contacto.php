7de3aba5f88563f838c6f2689cb74678eb333115
Kevin Jimenez <akevinieron@hotmail.com>
2012-10-26 07:15:51 -0400

    mitad del trabajo

---
+++ contacto.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Microfundicion FGLE</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/estilos.css">
+	<script type="text/javascript" src="js/live.js"></script>
+</head>
+<body>
+	<header>
+		<div class="container">
+			<!-- <img src="images/logo.png" alt="Logo Microfundicion FGLE"> -->
+			<div class="row">
+				<div class="logo span6">
+					<img src="images/logo.png" alt="Microfundicion FGLE Logo" />
+				</div>
+				<nav class="navbar span6">
+					<ul class="nav">
+						<li><a href="index.html">Inicio</a></li>
+						<li><a href="nosotros.html">Nosotros</a></li>
+						<li><a href="productos.html">Productos</a></li>
+						<li><a href="#">Contactos</a></li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+	</header>
+	<div class="container contenido">
+		<div class="row">
+			<div class="span10 offset1">
+				<h2>Contacto</h2>
+			</div>
+		</div>
+	</div>
+	<footer class="container">
+		<p>Todos los derechos reservados.</p>
+	</footer>
+</body>
+</html>
+
+
