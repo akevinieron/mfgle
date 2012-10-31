688984eff81223bab61099288eb811a66e67e8bd
Kevin Jimenez <akevinieron@hotmail.com>
2012-10-31 01:52:49 -0400

    Agregado fuente Overlock
    Agregue una fuente no comun utilizando Google Font Web, especificamente
    la Overlock

--- contacto.php
+++ contacto.php
@@ -19,7 +19,7 @@
 						<li><a href="index.html">Inicio</a></li>
 						<li><a href="nosotros.html">Nosotros</a></li>
 						<li><a href="productos.html">Productos</a></li>
-						<li><a href="#">Contactos</a></li>
+						<li><a href="#" class="current">Contactos</a></li>
 					</ul>
 				</nav>
 			</div>
